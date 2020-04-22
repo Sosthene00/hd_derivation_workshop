60b1581fd9f0b2c40ed8b524ce3374b635607a1c
Sosthene00 <674694@protonmail.ch>
2020-04-22 16:07:30 +0200

    ready for workshop

---
+++ hd_derivation_workshop/util.py
@@ -0,0 +1,14 @@
+def b58encode(v):
+    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+    p, acc = 1, 0
+    for c in reversed(v):
+        acc += p * c
+        p = p << 8
+
+    string = ""
+    while acc:
+        acc, idx = divmod(acc, 58)
+        string = alphabet[idx : idx + 1] + string
+    return string
+
